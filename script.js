9ec7de99ae9bdf1c1052615ec384e63cc17dfbb8
aleksander <83586282+cartographia@users.noreply.github.com>
2022-02-08 21:09:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var config = {
+    // Easily just add extensions bellow and the program will go through them
     "Documents": [
         "pdf",
         "doc",
@@ -35,80 +36,49 @@ var config = {
         "inf"
     ]
 };
+//--------------------------------------------------------------
+//get checkbox elements
 var checkDocuments = document.getElementById("searchDocuments");
 var checkDatabases = document.getElementById("searchDatabases");
 var checkSoftware = document.getElementById("searchSoftware");
-var counter = 0;
 
+var counter = 0; //global counter for blocked popups
+
+//get text placeholders
 var site = document.getElementById('site');
 var keyword = document.getElementById('keyword');
 
-function search(){
-    counter = 0;
-
-    if (!checkDocuments.checked && !checkDatabases.checked && !checkSoftware.checked){
-        alert("Looks like you haven't typed anything yet!");
+//--------------------------------------------------------------
+function search() {
+    counter = 0; //reset counter on every click 
+    console.log(counter);
+    //Checking if none of the checkboxes are checked and alerts the user
+    if (!checkDocuments.checked && !checkDatabases.checked && !checkSoftware.checked) {
+        alert("You have to check one option");
     }
 
-    if (checkDocuments.checked){search_documents();}
-    if (checkDatabases.checked){search_databases();}
-    if (checkSoftware.checked){search.software();}
+    //Depending on the checkbox checked, run that query with array provided
+    if (checkDocuments.checked) {
+        searchQuery(config.Documents);
+    }
+    if (checkDatabases.checked) {
+        searchQuery(config.Databases);
+    }
+    if (checkSoftware.checked) {
+        searchQuery(config.Software);
+    }
     // Check if it detected blocked popups
-    if (counter > 0){
-        alert(`Looks like ${counter} popups have been blocked, please check your browser settings and try again!`);
+    if (counter > 0) {
+        alert(`Our checker says ${counter} popups might have been blocked, please allow popups and try again!`);
     }
 }
-
-function search_documents() {
-    config.Documents.forEach(element => {
-        if(_hasPopupBlocker(window.open(`http://google.com/search?q=site%3A${site.value}+filetype%3A${element}+%22${keyword.value}%22`, "_blank"))){
-            counter += 1;
-        }
+function searchQuery(array) {
+    array.forEach(extension => {
+        isBlocked(window.open(`http://google.com/search?q=site%3A${site.value}+filetype%3A${extension}+%22${keyword.value}%22`, "_blank"));
     });
 }
-
-function search_databases() {
-    config.Databases.forEach(element => {
-        if(_hasPopupBlocker(window.open(`http://google.com/search?q=site%3A${site.value}+filetype%3A${element}+%22${keyword.value}%22`, "_blank"))){
-            counter += 1;
-        }
-    });
-}
-
-function search_software() {
-    config.Software.forEach(element => {
-        if(_hasPopupBlocker(window.open(`http://google.com/search?q=site%3A${site.value}+filetype%3A${element}+%22${keyword.value}%22`, "_blank"))){
-            counter += 1;
-        }
-    }); 
-}
-
-function _hasPopupBlocker(poppedWindow) {
-    var result = false;
-    try {
-        if (typeof poppedWindow == 'undefined') {
-            // Safari with popup blocker... leaves the popup window handle undefined
-            result = true;
-        }
-        else if (poppedWindow && poppedWindow.closed) {
-            // This happens if the user opens and closes the client window...
-            // Confusing because the handle is still available, but it's in a "closed" state.
-            // We're not saying that the window is not being blocked, we're just saying
-            // that the window has been closed before the test could be run.
-            result = false;
-        }
-        else if (poppedWindow && poppedWindow.test) {
-            // This is the actual test. The client window should be fine.
-            result = false;
-        }
-        else {
-            // Else we'll assume the window is not OK
-            result = true;
-        }
-    } catch (err) {
-        //if (console) {
-        //    console.warn("Could not access popup window", err);
-        //}
+function isBlocked(popupWindow){
+    if (!popupWindow || popupWindow.closed || typeof popupWindow.closed == 'undefined') {
+        counter += 1;
     }
-    return result;
 }
